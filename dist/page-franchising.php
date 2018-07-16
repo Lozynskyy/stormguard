ed4451ad135de322b6754efadab1f779be95249e
Vitaliy Lozynskiy <lozynskiy.v@gmail.com>
2018-07-16 08:38:31 +0300

    add page and page for franchising

---
+++ dist/page-franchising.php
@@ -0,0 +1,244 @@
+<?php /*Template Name: Franchising*/ ?>
+
+<?php get_header(); ?>
+
+<?php if(have_posts()) :
+
+    while (have_posts()) :
+
+        the_post();
+
+        $contact_block_title = get_field('contact_block_title');
+
+        $contact_block_description = get_field('contact_block_description');
+
+        $contact_block_button_1_text = get_field('contact_block_button_1_text');
+
+        $contact_block_button_2_text = get_field('contact_block_button_2_text');
+
+        $gallery_block_title = get_field('gallery_block_title');
+
+        $gallery_block_description = get_field('gallery_block_description');
+
+        $gallery_block_link_text = get_field('gallery_block_link_text');
+
+        $franchise_section_title = get_field('franchise_section_title');
+
+        $franchise_section_description = get_field('franchise_section_description');
+
+        $franchise_section_button_text = get_field('franchise_section_button_text');
+
+        $franchise_section_placeholder = get_field('franchise_section_placeholder');
+
+        $get_started_section_title = get_field('get_started_section_title');
+
+        $get_started_section_description = get_field('get_started_section_description');
+
+        $get_started_section_residential_link = get_field('get_started_section_residential_link');
+
+        $get_started_section_commercial_link = get_field('get_started_section_commercial_link');
+
+        $get_started_section_insurance_link = get_field('get_started_section_insurance_link');
+
+        $residential_link = get_page_link(83);
+
+        $commercial_link = get_page_link(85);
+
+        $insurance_link = get_page_link(87);
+
+    endwhile;
+
+endif; ?>
+
+<section class="breadcrumb">
+
+    <div class="breadcrumb__content">
+
+        <?php if(function_exists('bcn_display')){
+
+            bcn_display();
+
+        }?>
+
+    </div>
+
+</section>
+
+<section class="franchising">
+
+    <div class="franchising__content">
+
+        <?php if ( have_posts() ) :
+
+            while ( have_posts() ) :
+
+                the_post();
+
+                $image = get_field('page_image');
+
+                if( !empty($image) ): ?>
+
+                    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" class="franchising__image">
+
+                <?php endif; ?>
+
+                <?php
+
+                if( have_rows('articles') ):
+
+                    while ( have_rows('articles') ) : the_row();?>
+
+                        <h2 class="franchising__title">
+
+                            <?php the_sub_field('article_title');?>
+
+                        </h2>
+
+                        <p class="franchising__text">
+
+                            <?php the_sub_field('article_content');?>
+
+                        </p>
+
+                    <?php endwhile;
+
+                endif;
+
+                ?>
+
+            <?php endwhile;
+
+        endif; ?>
+
+    </div>
+
+    <div style="clear: both"></div>
+
+</section>
+
+<section class="find-franchise">
+
+    <div class="find-franchise__content find-franchise__content--fr">
+
+        <div class="find-franchise__content__item find-franchise__content__item--left">
+
+            <div class="find-franchise__text">
+
+                <p class="find-franchise__text__top">
+
+                    <?php echo $franchise_section_title; ?>
+
+                </p>
+
+                <p class="find-franchise__text__bot">
+
+                    <?php echo $franchise_section_description; ?>
+
+                </p>
+
+            </div>
+
+        </div>
+
+        <button class="ff-button ff-button--fr">
+
+            <?php echo $franchise_section_button_text ?>
+
+        </button>
+
+    </div>
+
+</section>
+
+<section class="contact-gallery">
+
+    <div class="contact-gallery__content">
+
+        <div class="contact-gallery__contact contact-gallery__contact--short contact-gallery__item">
+
+            <h2 class="contact-gallery__title">
+
+                <?php echo $contact_block_title; ?>
+
+            </h2>
+
+            <p class="contact-gallery__description">
+
+                <?php echo $contact_block_description; ?>
+
+            </p>
+
+            <button class="cg-contact-button">
+
+                <?php echo $contact_block_button_1_text; ?>
+
+            </button>
+
+            <button class="cg-contact-button">
+
+                <?php echo $contact_block_button_2_text; ?>
+
+            </button>
+
+        </div>
+        <div class="contact-gallery__gallery contact-gallery__item">
+
+            <h2 class="contact-gallery__title">
+
+                <?php echo $gallery_block_title; ?>
+
+            </h2>
+
+            <p class="contact-gallery__description">
+
+                <?php echo $gallery_block_description; ?>
+
+                <a href="<?php echo get_post_type_archive_link( 'gallery_item' ); ?>" class="contact-gallery__description__link">
+
+                    <?php echo $gallery_block_link_text; ?>
+
+                </a>
+
+            </p>
+
+            <div class="contact-gallery__gallery__body">
+
+                <?php $gallery_query = new WP_Query( array(
+
+                    'post_type' => 'gallery_item',
+
+                    'posts_per_page' => 8
+
+                ) );
+
+                if ( $gallery_query -> have_posts() ) :
+
+                    while ( $gallery_query -> have_posts() ) :
+
+                        $gallery_query -> the_post();
+
+                        $image = get_field('photo'); ?>
+
+                        <?php if( !empty($image) ): ?>
+
+                        <a href="<?php the_permalink(); ?>">
+
+                            <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" class="contact-gallery__gallery__item">
+
+                        </a>
+
+                    <?php endif;
+
+                    endwhile;
+
+                endif; ?>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+<?php get_footer(); ?>
