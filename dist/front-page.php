4e64fd7d4f89a59e5437e646b00571e7ca41b1b0
Vitaliy Lozynskiy <lozynskiy.v@gmail.com>
2018-07-23 16:14:11 +0300

    correct items positon in find-franchise section, add hover effect on submit button and add svg instead of png for icon

--- dist/front-page.php
+++ dist/front-page.php
@@ -68,7 +68,11 @@ endif; ?>
 
         <div class="find-franchise__content__item find-franchise__content__item--left">
 
-            <img src="<?php bloginfo('stylesheet_directory'); ?>/img/map-marker.png" alt="map-marker">
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="38" height="56" viewBox="0 0 38 56" class="find-franchise__icon">
+
+                <defs><path id="ug74a" d="M483 690.5c0 7-15.2 31.5-19 38.5-3.8-7-19-31.5-19-38.5 0-14 11.4-17.5 19-17.5s19 3.5 19 17.5zm-12.95 0c0-3.38-2.7-6.12-6.05-6.12a6.08 6.08 0 0 0-6.05 6.12c0 3.39 2.7 6.13 6.05 6.13a6.08 6.08 0 0 0 6.05-6.13z"/></defs><g><g transform="translate(-445 -673)"><use xlink:href="#ug74a"/></g></g>
+
+            </svg>
 
             <div class="find-franchise__text">
 
